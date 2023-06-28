994cdb98db996055baba0916bea34ddac141f041
Rosapedret2 <rosapedret2@gmail.com>
2023-06-28 11:03:45 +0200

    backgrounds y logo

--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Logo from '../../components/Logo.jsx'
+import './home.css'
 
 function Home() {
   return (
-    <div>
-      Home
+    <div className='home'>
+      <div >
+        <Logo/>
+      </div>
     </div>
   )
 }
