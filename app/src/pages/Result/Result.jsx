d1bbd56f678388596530feb9b06a8bb57daebfbe
mgblanco10 <mgblanco1001@gmail.com>
2023-06-28 16:44:41 +0200

    component card result

--- app/src/pages/Result/Result.jsx
+++ app/src/pages/Result/Result.jsx
@@ -3,12 +3,11 @@ import Logo from '../../components/Logo/Logo.jsx'
 import './result.css'
 import Cards from '../../components/Cards/Cards.jsx'
 
+
 function Result() {
   return (
     <div className='result'>
       <Logo/>
-      <Cards />
-      <Cards />
       <Cards />
     </div>
   )
