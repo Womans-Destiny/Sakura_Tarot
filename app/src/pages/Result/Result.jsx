ef02826fc62749f6a397d89444938c08d9042252
Rosapedret2 <rosapedret2@gmail.com>
2023-06-29 22:32:20 +0200

    arreglos css y componente Comment

--- app/src/pages/Result/Result.jsx
+++ app/src/pages/Result/Result.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Logo from '../../components/Logo/Logo.jsx'
 import './Result.css'
 import Cards from '../../components/Cards/Cards.jsx'
+import Comment from '../../components/Comment/Comment.jsx'
 
 
 function Result() {
@@ -9,6 +10,7 @@ function Result() {
     <div className='result'>
       <Logo/>
       <Cards />
+      <Comment />
      
     </div>
   )
