eff7a8256ca317558e944350e5a198e323d7d98f
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-06-29 15:56:02 +0200

    ..

--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 
-import Logo from '../../components/Logo.jsx'
+
 import Presentation from '../../components/Presentation/Presentation.jsx'
+import Logo from '../../components/Logo/Logo.jsx'
 import './home.css'
 import Button from '../../components/Button/Button.jsx'
 import './home.css'
