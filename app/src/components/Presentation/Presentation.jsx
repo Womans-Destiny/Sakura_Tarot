daf05dc508048f20311af1d7844fc256e8b2894e
Rosapedret2 <rosapedret2@gmail.com>
2023-06-29 16:38:39 +0200

    todo ok

---
+++ app/src/components/Presentation/Presentation.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Presentation.css';
+
+
+function Presentation() {
+  return (
+    <>
+    <div className='body-home'>
+      <h1>¡Bienvenid@!</h1>
+      <h3> Tirada de TAROT</h3>
+        <span>Nuestra tirada de Tarot es una tirada simple pasado, presente y futuro gratis de tres cartas.</span> 
+        <p><span>Carta N° 1: Pasado.</span>  Esta carta representa tus emociones y sentimientos y cómo te sentías emocionalmente en el pasado.</p>
+        <p><span> Carta N° 2: Presente.</span>  Esta carta es lo que te afecta en el entorno directo actual. La que en la que vives.</p>
+        <p><span> Carta N° 3:  Futuro. </span>  Esta carta está relacionada con la respuesta a la pregunta que has formulado respecto de tu futuro.</p>
+      <h2>¡Realiza tu tirada!</h2>
+    </div>
+</>
+  )
+}
+
+export default Presentation
--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Button from '../../components/Button/Button.jsx'
 import Logo from '../../components/Logo/Logo.jsx'
 import './home.css';
+import Presentation from '../../components/Presentation/Presentation.jsx'
 
 function Home() {
   return (
     <div className='home'>
         <Logo/>
+        <Presentation/>
         <Button />
       </div>
   )
