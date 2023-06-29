46a88b6fe1a3024a10f0807960ea7048fdf81816
mgblanco10 <mgblanco1001@gmail.com>
2023-06-29 07:31:07 +0200

    ventana modal de pagina de game, estilos DE RAMA FEATURE3 TRAIDOS

--- app/src/components/CardsReverse/CardsReverse.jsx
+++ app/src/components/CardsReverse/CardsReverse.jsx
@@ -1,71 +1,6 @@
-// import React, { useEffect, useState } from 'react';
-// import './CardsReverse.css';
-
-// function CardsReverse() {
-//     const [cards, setCards] = useState([]);
-//     const [showDeck, setShowDeck] = useState(true);
-//     const [selectedCards, setSelectedCards] = useState([]);
-
-//     useEffect(() => {
-//         fetch('https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/')
-//           .then(response => response.json())
-//           .then(data => {
-//             const updatedCards = data.map(({ cardsReverse }) => ({ sakuraReverse: cardsReverse.sakuraReverse }));
-//             setCards(updatedCards);
-//           })
-//           .catch(error => console.log(error));
-//       }, []);
-
-//       const handleDeckClick = () => {
-//         setShowDeck(false);
-//         setSelectedCards(cards.slice(0, 8));
-//       };
-
-//       const handleCardClick = (index) => {
-//         setSelectedCards(prevSelectedCards => {
-//           const updatedSelectedCards = [...prevSelectedCards];
-//           updatedSelectedCards.splice(index, 1);
-//           return updatedSelectedCards;
-//         });
-//       };
-
-//   return (
-//     <div className="card-reverse-container">
-//       {showDeck ? (
-//         <div
-//           className="card-reverse deck"
-//           onClick={handleDeckClick}
-//         >
-//           <span className='text-card-reverse'>Click para mostrar las cartas</span>
-//         </div>
-//       ) : (
-//         <>
-//           <div className="card-circle-reverse">
-//             {selectedCards.map((card, index) => (
-//               <div
-//                 className="card-reverse"
-//                 key={index}
-//                 style={{
-//                   transform: `rotate(${index * (360 / selectedCards.length)}deg) translateX(150px) rotate(-${index * (360 / selectedCards.length)}deg)`
-//                 }}
-//                 onClick={() => handleCardClick(index)}
-//               >
-//                 <img src={card.sakuraReverse} className="card-reverse-img-top" alt="carta sakura reverse" />
-//               </div>
-//             ))}
-//           </div>
-//         </>
-//       )}
-//     </div>
-//   )
-// }
-
-// export default CardsReverse
-
-
-
 import React, { useEffect, useState } from 'react';
 import './CardsReverse.css';
+import imgFeedback from '../../assets/img-feedback.png'
 
 function CardsReverse() {
   const [cards, setCards] = useState([]);
@@ -82,9 +17,18 @@ function CardsReverse() {
       .catch(error => console.log(error));
   }, []);
 
-  const handleDeckClick = () => {
-    setShowDeck(false);
-    setSelectedCards(cards.slice(0, 8));
+//   const handleDeckClick = () => {
+//     setShowDeck(false);
+//     setSelectedCards(cards.slice(0, 8));
+//   };
+const handleDeckClick = () => {
+    if (selectedCards.length === 0) {
+      setShowDeck(!showDeck);
+      setSelectedCards(cards.slice(0, 8));
+    } else {
+      setSelectedCards([]);
+      setShowDeck(true);
+    }
   };
 
   const handleCardClick = (index) => {
@@ -100,6 +44,8 @@ function CardsReverse() {
   };
 
   return (
+    <>
+    <h3 className='title-game'>ESCOGE TRES CARTAS</h3>
     <div className="card-reverse-container">
       {showDeck ? (
         <div className="card-reverse deck" onClick={handleDeckClick}>
@@ -121,9 +67,16 @@ function CardsReverse() {
               </div>
             ))}
           </div>
+          {selectedCards.length === 5 && (
+            <div className="ready-text">
+                <img className='image-feedback' src={imgFeedback} alt='imagen de la ventana modal del feedback' />
+                <p className='text-feedback'>TU TIRADA HA SIDO GUARDADA</p>
+            </div>
+          )}
         </>
       )}
     </div>
+    </>
   );
 }
 
--- app/src/components/Logo/Logo.jsx
+++ app/src/components/Logo/Logo.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import logo from '../../assets/logo_desktop.svg'
 import './Logo.css'
+import { Link } from 'react-router-dom';
 
 function Logo() {
   return (
-    <div>
+    <Link to="/">
       <img className="logo-img" src={logo} alt="Logo Womans Destiny" />
-    </div>
+    </Link>
   )
 }
 
