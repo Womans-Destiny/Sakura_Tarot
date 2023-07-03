246f5b995e10f7aa9670021caf38e49d4d0f46ec
Rita7979 <131756045+Rita7979@users.noreply.github.com>
2023-07-03 10:30:33 +0200

    Cambio

--- app/src/components/Window/window.jsx
+++ app/src/components/Window/window.jsx
@@ -1,42 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react'
+import './Window.css';
 
-const Window = () => {
-  const [meaning, setMeaning] = useState('');
-  const [comments, setComments] = useState([]);
-  const [newComment, setNewComment] = useState('');
-
-  const handleSaveComment = () => {
-    setComments([...comments, newComment]);
-    setNewComment('');
-  };
-
-  const date = new Date().toLocaleDateString(); 
-
+function Window() {
+    const date = new Date().toLocaleDateString();
+    const reading = 'Tu lectura';
+    const comment = 'Tu comentario';
   return (
-    <div className="window">
-      <div className="window-content">
-        <h3>{date}</h3>
-        <p>{meaning}</p>
-        <p>Tu comentario</p>
-        <div
-          className="comment-input"
-          contentEditable="true"
-          onInput={(e) => setNewComment(e.target.textContent)}
-        ></div>
-        <button onClick={handleSaveComment}>Guardar comentario</button>
-      </div>
-
-      {comments.map((comment, index) => (
-        <div className="window" key={index}>
-          <div className="window-content">
-            <h3>{date}</h3>
-            <p>{meaning}</p>
-            <p>{comment}</p>
-          </div>
-        </div>
-      ))}
+<div className="window">
+    <div className="window-content">
+      <h3>{date}</h3>
+      <p>{reading}</p>
+      <p>{comment}</p>
     </div>
-  );
-};
+  </div>
+  )
+}
 
-export default Window;
+export default Window
