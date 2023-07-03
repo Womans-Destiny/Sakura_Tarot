bcee8b283fdc17625e11823b9cc1ba8254335f0f
LucíaSilva <coderalsc@gmail.com>
2023-07-03 12:39:08 +0200

    cambios en boton de favoritos para solucionar conflictos Marga)

--- app/src/pages/Result/Result.jsx
+++ app/src/pages/Result/Result.jsx
@@ -3,12 +3,15 @@ import Logo from '../../components/Logo/Logo.jsx'
 import './Result.css'
 import Cards from '../../components/Cards/Cards.jsx'
 import ButtonFavorites from '../../components/ButtonFavorites/ButtonFavorites.jsx'
+import StarD from '../../components/starD/StarD.jsx'
+import ButtonRestart from '../../components/ButtonRestart/ButtonRestart.jsx'
+
 
 function Result() {
   return (
     <div className='result'>
       <Logo/>
-      <StarD/>
+      <StarD />
       <Cards />
       <ButtonFavorites />
       <ButtonRestart />
