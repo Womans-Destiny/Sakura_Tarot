25d4af5137b3ea275df04cb864e108f51301c54b
LucíaSilva <coderalsc@gmail.com>
2023-07-04 11:46:11 +0200

    css presentation, clases desconocidas

--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Presentation from '../../components/Presentation/Presentation.jsx'
 import Logo from '../../components/Logo/Logo.jsx'
 import './home.css'
+import '../../components/Presentation/Presentation.css'
 import Button from '../../components/Button/Button.jsx'
 
 
