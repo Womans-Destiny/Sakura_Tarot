7afa6a6f272f0322a7fc562ce1205ef643496dd2
mgblanco10 <mgblanco1001@gmail.com>
2023-07-05 19:12:05 +0200

    changes of background branch feature3

--- app/src/pages/Favorites/Favorites.jsx
+++ app/src/pages/Favorites/Favorites.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import './favorites.css'
 import Logo from '../../components/Logo/Logo.jsx'
 import Window from '../../components/Window/Window'
-import StarD from '../../components/StarD/StarD'
 
 function Favorites() {
   return (
     <div className='favorites'>
       <Logo/>
-      <StarD />
       <Window />
     </div>
   )
