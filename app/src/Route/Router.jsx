67c39a40036b55726d71be6078fba782406e455f
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-07-06 14:05:41 +0200

    comment

--- app/src/Route/Router.jsx
+++ app/src/Route/Router.jsx
@@ -1,22 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from '../App';
-import '../index.css';
-import Home from '../pages/Home/Home';
-import Result from '../pages/Result/Result.jsx';
-import Game from '../pages/Game/Game.jsx';
-import Favorites from '../pages/Favorites/Favorites.jsx';
 
+import { BrowserRouter, Route } from 'react-router-dom';
+import App from '../App'
+import '../index.css'
+import Result from '../pages/Result/Result.jsx'
+import Game from '../pages/Game/Game.jsx'
+import Favorites from '../pages/Favorites/Favorites.jsx'
+import Home from '../pages/Home/Home';
 
 function Router() {
   return (
     <BrowserRouter>
-      <Routes>
+      <Route>
         <Route path='/' element={<App />}/>
         <Route path='/home' element={<Home />}/>
         <Route path='/result' element={<Result />}/>
         <Route path='/game' element={<Game />}/>
         <Route path='/favorites' element={<Favorites />}/>
-      </Routes>
+      </Route>
     </BrowserRouter>
   )}
 
