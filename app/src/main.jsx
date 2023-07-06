8ddd898fb93049ed95ca3fa5a1eb315705b75506
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-07-06 14:26:53 +0200

    routes

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
-import { Router } from './routes/Router';
+import { Router } from './Routes/Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
